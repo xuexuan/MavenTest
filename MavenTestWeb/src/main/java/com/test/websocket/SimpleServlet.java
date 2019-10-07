0e01c6368735e1af3d93b89eab28a8a5794d2711
Orz.KR <25887708@qq.com>
2019-10-07 13:13:17 +0800

    Maven + spring boot to build a hello world web app

---
+++ MavenTestWeb/src/main/java/com/test/websocket/SimpleServlet.java
@@ -0,0 +1,25 @@
+package com.test.websocket;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+/**
+ * Servlet implementation class SimpleServlet
+ */
+@WebServlet(urlPatterns = "*/hello/*")
+public class SimpleServlet extends HttpServlet {
+   private static final long serialVersionUID = 1L;
+   @Override
+   protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+	   
+		   	resp.setContentType("text/plain");
+		   	resp.getWriter().write("Hello World! Maven Web Project Example.");
+	
+   }
+}
